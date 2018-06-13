5f156c482ec0d625923a218f3b9e1dce8bce6dad
unknown <prakharsaxena2@gmail.com>
2018-06-13 20:24:40 +0530

    Assignment2 added

---
+++ Assignment2.py
@@ -0,0 +1,36 @@
+>>> a= "Hello everyone"
+>>> print(a)
+Hello everyone
+>>> print("Hello everyone")
+Hello everyone
+>>> b ="Acad"
+>>> c ="View"
+>>> b + c
+>>> input("Enter x")
+Enter x2
+'2'
+>>> input("Enter y ")
+Enter y 3
+'3'
+>>> input("Enter z ")
+Enter z 5
+'5'
+>>> print("\"Let\'s get started\"")
+"Let's get started"
+>>> print("\"Let\'s get started\"on screen")
+"Let's get started"on screen
+>>> print("s=\"Acadview\"")
+s="Acadview"
+>>> print("course=\"Python\"")
+course="Python"
+>>> print("free=5000")
+free=5000
+>>> name ="Tony Stack"
+>>> salary= 1000000
+>>> print(name + salary)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: must be str, not int
+>>> print(name,salary)
+Tony Stack 1000000
+>>>
