19866a1a7b2326574c339c033c109c2a6911b5ce
unknown <prakharsaxena2@gmail.com>
2018-06-16 00:27:05 +0530

    Assignemt 1 is added and updated

---
+++ Assignment1.py
@@ -0,0 +1,23 @@
+print("Hello World")  # printing Hello World
+
+
+print("Acad" + "View")  # "Acad" + "View"  joining to string
+
+x = input("Enter the value of x")
+y = input("Enter the value of y")  #Using input ststement
+z = input("Enter the value of z")
+
+print("Values of x",x)
+print("Values of x",y) #printing values
+print("Values of x",z)
+
+print("\"Let's get started\"") # printing "Let's get started"
+
+s = "Acadview"
+course = "Python"  #printing values using placeholder
+fees = 5000
+print(('%s"%s"%d')%(s,course,fees))
+
+name = "Tony"
+salary = 1000000
+print(('%s %d')%(name,salary))
